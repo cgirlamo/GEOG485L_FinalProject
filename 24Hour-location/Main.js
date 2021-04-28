b996ac650f1ca9de4ec0b7ce569ec0bc31ddbc83
Chris Girlamo <cgirlamo@unm.edugit config --global user.email cgirlamo@unm.edu>
2021-04-28 15:04:41 -0600

    add in form data

--- 24Hour-location/Main.js
+++ 24Hour-location/Main.js
@@ -1,7 +1,7 @@
 var mymap = L.map("mapid").setView([40.91443, -74.17075], 12);
 
-var Pcoords = [];
-var Rcoords2 = [];
+var Pcoords = {};
+var Rcoords2 = {};
 //add in the basemap
 var Esri_WorldStreetMap = L.tileLayer(
   "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}",
@@ -14,6 +14,8 @@ var attributelist = [];
 var loctype;
 var named;
 var frequency;
+var keyid = 0;
+var feat = {};
 
 //create the feature group to be drawn and add the mto the map
 var drawnItems = new L.FeatureGroup();
@@ -44,7 +46,7 @@ mymap.on(L.Draw.Event.CREATED, function (e) {
   if (type == "marker") {
     latlngs = layer.getLatLng();
     var values = Object.values(latlngs);
-    Pcoords.push(values);
+    Pcoords[keyid] = values;
   }
   if (type == "rectangle") {
     var values = [];
@@ -59,16 +61,16 @@ mymap.on(L.Draw.Event.CREATED, function (e) {
       }
       Rcoords.push(values);
     }
-    Rcoords2.push(Rcoords);
+    Rcoords2[keyid] = Rcoords;
   }
   //create a popup with a submit button to be created with the marker
   var popupContent =
     '<form id = "form1">' +
     '<div class = "form-group"' +
-    '<div style = "text-align:center;" class = "col-xs-4"><input type = "text" id = "LocType" placeholder = "Location Type"/></div>' +
-    '<div style = "text-align:center;" class = "col-xs-4"><input type = "text" id = "Name" placeholder = "Name"/></div>' + 
-    '<div style = "text-align:center;" class = "col-xs-4"><input type = number id = "frequency" placeholder = "Frequency per week"/></div>' +
-    '<div style = "text-align:center; class = "col-xs-4><button type = "button" id="submit2" class = "btn btn-secondary">Submit Feature</button>'
+    '<div style = "text-align:center;" class = "col-xs-4"><input type = "text" id = "LocType"  placeholder = "Location Type"/></div>' +
+    '<div style = "text-align:center;" class = "col-xs-4"><input type = "text" id = "Name"  placeholder = "Name"/></div>' + 
+    '<div style = "text-align:center;" class = "col-xs-4"><input type = number id = "frequency" value = 0 placeholder = "Frequency per week"/></div>' +
+    '<div style = "text-align:center;" class = "col-xs-4"><button type = "submit" id="sub2" class = "btn btn-secondary">Submit Feature</button></div>'
     "</div>" +
     "</form>";
   //add the popup
@@ -78,59 +80,118 @@ mymap.on(L.Draw.Event.CREATED, function (e) {
       closeOnClick: true,
     })
     .openPopup();
+    $("#sub2").on('click', function(e) {
 
+      e.preventDefault();
+      console.log('I work')
+      loctype = $('#LocType').val();
+      named = $('#Name').val();
+      frequency = $("#frequency").val();
+      feat[keyid] = [loctype,named,frequency];
+      console.log(feat[keyid])
+      keyid = keyid + 1;
+      layer.closePopup();
+  });
 });
-$("#submit2").on('click', function(e) {
 
-    e.preventDefault();
-    console.log('I work')
-    loctype = $('#LocType').val();
-    named = $('#Name').val();
-    frequency = $("#frequency").val();
-    attributelist.push([loctype,named,frequency]);
-    console.log(attributelist);
-})
 var features = [];
 var featureClass = {};
 $("#submit").on("click", function (e) {
 
-  for (feature in Rcoords2) {
-    var geojson = {};
-    geojson["type"] = "Feature";
-    geojson["geometry"] = {};
-    geojson["geometry"]["type"] = "Polygon";
-    geojson["properties"] = {};
-    geojson["properties"]["loctype"] = attributelist[feature][0];
-    geojson["properties"]["named"] = attributelist[feature][1];
-    geojson["properties"]["frequency"] = attributelist[feature][2];
-    var newcoords = [];
-    geojson["properties"]["ID"] = $("#ID").val();
-    for (var x = 0; x < Rcoords2[feature].length; x++) {
-      var coordinates = Rcoords2[feature][x];
-      console.log(coordinates);
-      newcoords.push([coordinates[1], coordinates[0]]);
-    }
-    geojson["geometry"]["coordinates"] = [newcoords];
-    features.push(geojson);
-  }
-  console.log(Pcoords);
-  for (feature in Pcoords) {
-    coord = Pcoords[feature];
-    var geojson = {};
-    geojson["type"] = "Feature";
-    geojson["geometry"] = {};
-    geojson["properties"] = {};
-    geojson["geometry"]["type"] = "Point";
-    geojson["geometry"]["coordinates"] = [coord[1], coord[0]];
-    geojson["properties"]["ID"] = $("#ID").val();
-    geojson["properties"]["loctype"] = attributelist[feature][0];
-    geojson["properties"]["named"] = attributelist[feature][1];
-    geojson["properties"]["frequency"] = attributelist[feature][2];
+for (key in Object.keys(feat)) {
+  console.log(key)
 
-    features.push(geojson);
+
+  var geojson = {};
+  geojson["type"] = "Feature";
+  geojson["geometry"] = {};
+  for (pkey in Object.keys(Pcoords)) {
+    if(pkey == key) {
+      coord = Pcoords[key]
+      geojson['geometry']['type'] = 'Point';
+      geojson['properties'] = {};
+      geojson['properties']['FID'] = key;
+      geojson['properties']['loctype'] = feat[key][0];
+      geojson['properties']['named'] = feat[key][1];
+      geojson['properties']['frequency'] = feat[key][2];
+      geojson["geometry"]["coordinates"] = [coord[1], coord[0]];
+      geojson['properties']['email'] = $("#ID").val();
+
+    }
+    features.push(geojson)
   }
-  featureClass["type"] = "FeatureCollection";
-  featureClass["features"] = features;
+  for (rkey in Rcoords2) {
+    if(rkey == key) {
+      geojson['type'] = 'Feature';
+      geojson['geometry'] = {};
+      geojson['geometry']['type'] = 'Polygon';
+      geojson['properties'] = {};
+      geojson['properties']['loctype'] = feat[key][0];
+      geojson['properties']['named'] = feat[key][1];
+      geojson['properties']['frequency'] = feat[key][2];
+      var newcoords = [];
+      geojson['properties']['email'] = $("#ID").val();
+      for (var x= 0; x < Rcoords2[key].length; x++) {
+        var xy;
+        
+        if(x == Rcoords2[key].length) {
+          xy = Rcoords2[key][0];
+          newcoords.push([xy[1],xy[0]]);
+        } else {
+          xy = Rcoords2[key][x];
+          newcoords.push([xy[1],xy[0]]);
+      }
+      
+     }
+     geojson['geometry']['coordinates'] = newcoords
+     features.push(geojson)
+  }
+  
+  featureClass['type'] = 'FeatureCollection';
+  featureClass['features'] = features;
   var json = JSON.stringify(featureClass);
   $("#geocode").val(json);
+  
+
+};
+};
+  // for (feature in Rcoords2) {
+  //   var geojson = {};
+  //   geojson["type"] = "Feature";
+  //   geojson["geometry"] = {};
+  //   geojson["geometry"]["type"] = "Polygon";
+  //   geojson["properties"] = {};
+  //   geojson["properties"]["loctype"] = attributelist[feature][0];
+  //   geojson["properties"]["named"] = attributelist[feature][1];
+  //   geojson["properties"]["frequency"] = attributelist[feature][2];
+  //   var newcoords = [];
+  //   geojson["properties"]["ID"] = $("#ID").val();
+  //   for (var x = 0; x < Rcoords2[feature].length; x++) {
+  //     var coordinates = Rcoords2[feature][x];
+  //     console.log(coordinates);
+  //     newcoords.push([coordinates[1], coordinates[0]]);
+  //   }
+  //   geojson["geometry"]["coordinates"] = [newcoords];
+  //   features.push(geojson);
+  // }
+  // console.log(Pcoords);
+  // for (feature in Pcoords) {
+  //   coord = Pcoords[feature];
+  //   var geojson = {};
+  //   geojson["type"] = "Feature";
+  //   geojson["geometry"] = {};
+  //   geojson["properties"] = {};
+  //   geojson["geometry"]["type"] = "Point";
+  //   geojson["geometry"]["coordinates"] = [coord[1], coord[0]];
+  //   geojson["properties"]["ID"] = $("#ID").val();
+  //   geojson["properties"]["loctype"] = attributelist[feature][0];
+  //   geojson["properties"]["named"] = attributelist[feature][1];
+  //   geojson["properties"]["frequency"] = attributelist[feature][2];
+
+  //   features.push(geojson);
+  // }
+  // featureClass["type"] = "FeatureCollection";
+  // featureClass["features"] = features;
+  // var json = JSON.stringify(featureClass);
+  // $("#geocode").val(json);
 });
